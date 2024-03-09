d5f7d701769c50aa89d079f8a3874cf4a5e34a16
aman <amansomething@gmail.com>
2024-03-09 15:19:57 -0500

    Added get_data function

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
+    path("get_data", views.get_data, name="get_data"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 
+import httpx
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://us.pycon.org/2024/schedule/"
+HTML_FILE = "schedule.html"
+
+
+def get_data(request):
+    response = httpx.get(BASE_URL)
+    if response.status_code != 200:
+        context = {
+            "message": "Issue refreshing data!"
+        }
+    else:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        with open(HTML_FILE, 'w', encoding='utf-8') as file:
+            file.write(str(soup))
+
+        context = {
+            "message": "Latest schedule data downloaded successfully! Ready to parse."
+        }
+
+    return render(request, "index.html", context=context)
+
 
 def index(request):
     return render(request, "index.html")
