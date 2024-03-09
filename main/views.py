2b72f03d30356efec14611283feba50fbce8da40
aman <amansomething@gmail.com>
2024-03-09 15:46:26 -0500

    feat: Started parse_data function and view

--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from django.shortcuts import render
+from django.core.handlers.wsgi import WSGIRequest
 
 import httpx
 from bs4 import BeautifulSoup
@@ -7,7 +10,24 @@ BASE_URL = "https://us.pycon.org/2024/schedule/"
 HTML_FILE = "schedule.html"
 
 
-def get_data(request):
+def read_html_file(file_path: str = HTML_FILE) -> Optional[BeautifulSoup]:
+    """
+    Reads the HTML file returns the contents as a soup object to be used by other functions.
+    """
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+            soup = BeautifulSoup(content, 'html.parser')
+            return soup
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        return None
+
+
+def get_data(request: WSGIRequest) -> render:
+    """
+    Gets the latest data from the website and saves the results.
+    """
     response = httpx.get(BASE_URL)
     if response.status_code != 200:
         context = {
@@ -25,5 +45,19 @@ def get_data(request):
     return render(request, "index.html", context=context)
 
 
+def parse_data(request: WSGIRequest) -> render:
+    """
+    Parses the schedule data and displays the results.
+    """
+    soup = read_html_file()
+    headings = [x.get_text() for x in soup.find_all("h3")]
+    print(headings)
+
+    return render(request, "index.html", context={"headings": headings})
+
+
 def index(request):
+    """
+    Loads the home page.
+    """
     return render(request, "index.html")
