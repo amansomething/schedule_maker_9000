29e6abda10c315d327e163ec0f85a0dd5a34928a
aman <amansomething@gmail.com>
2024-03-10 10:25:56 -0400

    feat: Get links for app presentations

--- main/views.py
+++ main/views.py
@@ -6,11 +6,12 @@ from django.core.handlers.wsgi import WSGIRequest
 import httpx
 from bs4 import BeautifulSoup
 
-BASE_URL = "https://us.pycon.org/2024/schedule/"
-HTML_FILE = "schedule.html"
+BASE_URL = "https://us.pycon.org"
+SCHEDULES_URL = f"{BASE_URL}/2024/schedule/"
+RESULTS_DIR = "site_data"
 
 
-def read_html_file(file_path: str = HTML_FILE) -> Optional[BeautifulSoup]:
+def read_html_file(file_path) -> Optional[BeautifulSoup]:
     """
     Reads the HTML file returns the contents as a soup object to be used by other functions.
     """
@@ -24,36 +25,55 @@ def read_html_file(file_path: str = HTML_FILE) -> Optional[BeautifulSoup]:
         return None
 
 
+def get_all_schedule_links() -> list[str]:
+    """
+    Gets all the schedule links from the main schedule page.
+
+    :return: A list of schedule links.
+    """
+    response = httpx.get(BASE_URL + "/2024/schedule/")
+    if response.status_code != 200:
+        return []
+
+    results = []
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    prefix = "/2024/schedule/presentation/"
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href and href.startswith(prefix):
+            results.append(href)
+
+    for item in results:
+        link = BASE_URL + item
+        filename = item.split("/")[-2].strip() + ".html"
+        print(f"{link} => {filename}")
+        # response = httpx.get(link)
+        # file_content_str = str(BeautifulSoup(response.content, 'html.parser'))
+        #
+        # with open(f"{RESULTS_DIR}/{filename}", 'w', encoding='utf-8') as file:
+        #     file.write(file_content_str)
+
+    return results
+
+
 def get_data(request: WSGIRequest) -> render:
     """
     Gets the latest data from the website and saves the results.
     """
-    response = httpx.get(BASE_URL)
-    if response.status_code != 200:
-        context = {
-            "message": "Issue refreshing data!"
-        }
-    else:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        with open(HTML_FILE, 'w', encoding='utf-8') as file:
-            file.write(str(soup))
+    links = get_all_schedule_links()
 
-        context = {
-            "message": "Latest schedule data downloaded successfully! Ready to parse."
-        }
+    if links:
+        message = "Latest schedule data downloaded successfully! Ready to parse."
+    else:
+        message = "Error downloading schedule data. Please try again later."
+
+    context = {
+        "message": message,
+        "results": links,
+    }
 
     return render(request, "index.html", context=context)
-
-
-def parse_data(request: WSGIRequest) -> render:
-    """
-    Parses the schedule data and displays the results.
-    """
-    soup = read_html_file()
-    headings = [x.get_text() for x in soup.find_all("h3")]
-    print(headings)
-
-    return render(request, "index.html", context={"headings": headings})
 
 
 def index(request):
