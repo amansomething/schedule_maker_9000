e9ea64361a8de85a804438ee74fbd2103dcd1d4f
aman <amansomething@gmail.com>
2024-03-10 21:05:57 -0400

    feat: Models for Presenter and Event

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Presenter(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    timestamp = models.DateTimeField()
+    presenters = models.ManyToManyField(Presenter)
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
