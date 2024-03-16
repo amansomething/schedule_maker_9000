394080f8b793a9fa3c31870197b81ab8f4ea50aa
aman <amansomething@gmail.com>
2024-03-16 08:34:05 -0400

    feat: Added TableUpdate model

--- main/models.py
+++ main/models.py
@@ -21,3 +21,11 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class TableUpdate(models.Model):
+    table_name = models.CharField(max_length=255, unique=True)
+    last_updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Last update: {self.last_updated}"
