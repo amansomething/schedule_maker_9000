1b697a48ea92b3987f6fedd8f7e8e7599ee030cd
aman <amansomething@gmail.com>
2024-03-16 08:59:20 -0400

    fix: TableUpdate model. Also added to admin

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Presenter, Event
+from .models import Presenter, Event, TableUpdate
 
 admin.site.register(Presenter)
 admin.site.register(Event)
+admin.site.register(TableUpdate)
--- main/models.py
+++ main/models.py
@@ -24,8 +24,8 @@ class Event(models.Model):
 
 
 class TableUpdate(models.Model):
-    table_name = models.CharField(max_length=255, unique=True)
-    last_updated = models.DateTimeField(auto_now_add=True)
+    table_name = models.CharField(max_length=255)
+    last_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Last update: {self.last_updated}"
