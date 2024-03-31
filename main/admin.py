872f46e14351e3562e6a296359f1a5a03c9de40b
aman <amansomething@gmail.com>
2024-03-31 16:46:21 -0400

    Updated TableUpdates admin view

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Presenter, Event, TableUpdate
 
+
+class TableUpdateAdmin(admin.ModelAdmin):
+    list_display = ("table_name", "last_updated")
+
+
 admin.site.register(Presenter)
 admin.site.register(Event)
-admin.site.register(TableUpdate)
+admin.site.register(TableUpdate, TableUpdateAdmin)
