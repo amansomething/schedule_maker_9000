f2784f4298774859ba219ebf59bb5a8a509a6e53
aman <amansomething@gmail.com>
2024-04-01 12:15:01 -0400

    Admin view shows better Event details

--- main/admin.py
+++ main/admin.py
@@ -7,6 +7,10 @@ class TableUpdateAdmin(admin.ModelAdmin):
     list_display = ("table_name", "last_updated")
 
 
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("title", "start_time", "end_time", "location")
+
+
 admin.site.register(Presenter)
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
 admin.site.register(TableUpdate, TableUpdateAdmin)
